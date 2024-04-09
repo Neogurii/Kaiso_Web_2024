308a16846c468345f9d4a8f522020af688bf3c04
Isaac_L <dizack10@gmail.com>
2024-04-09 16:12:48 +0200

    Test icon display

--- docs/index.4519d957.js
+++ docs/index.4519d957.js
@@ -1,5 +1,4 @@
-const template = document.createElement("template");
-template.innerHTML = `
+const e=document.createElement("template");e.innerHTML=`
 <footer style="display: flex; flex-direction: column; align-items: center;">
 <div class="margin footer_info" style="display: flex; justify-content: space-between;">
     <a class="logo" href="./index.html"></a>
@@ -8,12 +7,12 @@ template.innerHTML = `
         <ul>
             <li>Design en entreprise</li>
             <li>Jeux mobiles</li>
-            <li>Veille & Idéation</li>
+            <li>Veille & Id\xe9ation</li>
         </ul>
     </section>
 
     <section>
-        <h5>À propos</h5>
+        <h5>\xc0 propos</h5>
         <ul>
             <li>De nous</li>
             <li>De l'entreprise</li>
@@ -29,7 +28,7 @@ template.innerHTML = `
             <a target="_blank" rel="noopener noreferrer" href="mailto:contact@kaisostudio.org" class="icon" style="background-image: url("./Assets/Icons/Mail_black.svg");">
         </article>
 
-<p>Kaiso Studio © 2024</p>
+<p>Kaiso Studio \xa9 2024</p>
 </footer>
 
 <style>
@@ -61,5 +60,4 @@ template.innerHTML = `
     }
 
 </style>
-`
-document.body.appendChild(template.content)
+`,document.body.appendChild(e.content);
